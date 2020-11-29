961ff30b72d0df6ef101618e0c08ad7523a23f96
^^ Bianca ^^ <73293685+XxBiancaXx@users.noreply.github.com>
2020-11-29 14:58:47 +0100

    Create ban-unban.py

---
+++ ban-unban.py
@@ -0,0 +1,23 @@
+@bot.command()
+@has_permissions(ban_members=True)
+async def ban(ctx, member : discord.Member, *, reason = None):
+    if reason == None:
+        reason = 'Because you were too cutie ^^'
+
+    await ctx.send(f'Banning {member} with the reason **`{reason}`**')
+    await member.ban(reason = reason)
+
+@bot.command()
+@has_permissions(administrator=True)
+async def unban(ctx, id: int = None):
+    if id == None:
+        await ctx.send('Oopsies but you need an ID!')
+
+    else:
+        if len(str(id)) == 18:
+            user = await bot.fetch_user(id)
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned **{id}**! Welcome back buddy!')
+
+        else:
+            await ctx.send(f'Oops! **{id}** doesn\'t seem like a valid ID! Try again!')
